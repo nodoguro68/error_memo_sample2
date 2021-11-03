441e34d59c09c31967632540a796355f85b27c3e
kawada68 <nodoguro68@gmail.com>
2021-11-03 18:14:49 +0900

    使い回す機能をまとめる

---
+++ src/utility/utility.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once './debug.php';
+require_once './log_config.php';
+require_once './escape.php';
+require_once './dbConnect.php';
+require_once './db.php';
+require_once './session.php';
+require_once './validation.php';
