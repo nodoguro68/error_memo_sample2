b0db9c23a7a72dc8b4ebc18a0c8d2e59c0f6dd15
kawada68 <nodoguro68@gmail.com>
2021-11-04 00:23:51 +0900

    fetchUserDataByMailAddress関数とlogin関数を作成

--- src/function/user.php
+++ src/function/user.php
@@ -37,3 +37,63 @@ function createUser(&$err_msg, $mail_address, $password)
         $err_msg['common'] = ERR_MSG;
     }
 }
+
+
+/**
+ * メールアドレスを使ってユーザーのデータを取得
+ * 
+ * @param array $err_msg
+ * @param string $column
+ * @param string $mail_address
+ * @return mixed
+ */
+function fetchUserDataByMailAddress(&$err_msg, $column, $mail_address)
+{
+
+    try {
+
+        $dbh = dbConnect();
+
+        $sql = 'SELECT ' . $column . ' FROM users WHERE mail_address = :mail_address AND is_deleted = 0';
+        $data = array(':mail_address' => $mail_address);
+
+        $user_data = fetch($dbh, $sql, $data);
+        return $user_data;
+
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
+/**
+ * ログイン
+ * 
+ * @param array $err_msg
+ * @param string $mail_address
+ * @param string $password
+ * @param bool $pass_save
+ */
+
+function login(&$err_msg, $mail_address, $password, $pass_save)
+{
+
+    $user_data = fetchUserDataByMailAddress($err_msg, 'user_id,password', $mail_address);
+
+    if (!empty($user_data) && password_verify($password, $user_data['password'])) {
+
+        $session_limit = 60 * 60;
+        $_SESSION['login_date'] = time();
+        $_SESSION['user_id'] = (int)$user_data['user_id'];
+        
+        if ($pass_save) {
+            $_SESSION['login_limit'] = $session_limit * 24 * 30;
+        } else {
+            $_SESSION['login_limit'] = $session_limit;
+
+        }
+
+    } else {
+        $err_msg['common'] = ERR_MSG_LOGIN;
+    }
+}
