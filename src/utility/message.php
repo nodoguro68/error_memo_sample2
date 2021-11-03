89336b9cb5846a1f9c38028c548893bdc694f6d4
kawada68 <nodoguro68@gmail.com>
2021-11-03 15:53:59 +0900

    エラーメッセージの配列とメッセージ定数作成

---
+++ src/utility/message.php
@@ -0,0 +1,29 @@
+<?php 
+
+$err_msg = array();
+
+const ERR_MSG = 'エラーが発生しました。しばらく経ってからやり直してください。';
+const ERR_MSG_REQUIER = '入力必須です';
+const ERR_MSG_DB_CONNECT = 'データベースへの接続に失敗しました';
+const ERR_MSG_LOGIN = 'メールアドレスまたはパスワードが違います';
+const ERR_MSG_MAX_LEN = '文字以内で入力してください';
+const ERR_MSG_MIN_LEN = '文字以上で入力してください';
+const ERR_MSG_MAIL = 'メールアドレスの形式で入力してください';
+const ERR_MSG_MAIL_DUP = '既に登録されているメールアドレスです';
+const ERR_MSG_PASS_RE = 'パスワードの再入力が違います';
+const ERR_MSG_PASS_OLD = '古いパスワードが違います';
+const ERR_MSG_PASS_OLD_DUP = '古いパスワードと同じです';
+const ERR_MSG_HALF = '半角英数字のみご利用いただけます';
+const ERR_MSG_EXPIRE = '有効期限が切れています';
+const ERR_MSG_SEND_MAIL = 'メールの送信に失敗しました';
+const ERR_MSG_MATCH = '正しくありません';
+const ERR_MSG_LENGTH = '文字で入力してください';
+const SUC_MSG_REGISTER_USER = 'ユーザー登録が完了しました';
+const SUC_MSG_LOGIN = 'ログインしました';
+const SUC_MSG_LOGOUT = 'ログアウトされました';
+const SUC_MSG_UPDATE_PROFILE = 'プロフィールを更新しました';
+const SUC_MSG_UPDATE = '更新しました';
+const SUC_MSG_REGISTER = '登録しました';
+const SUC_MSG_DELETE = '削除しました';
+const SUC_MSG_MAIL = 'メールを送信しました';
+const SUC_MSG_UPDATE_PASS = 'パスワードを変更しました';
