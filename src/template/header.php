eac346329b3b0cc5ea7eeb68351e16ab70c6746b
kawada68 <nodoguro68@gmail.com>
2021-11-03 18:54:06 +0900

    headerタグのテンプレート作成

---
+++ src/template/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <div class="header__inner">
+        <h1 class="header__title"><a href="index.php"></a></h1>
+        <nav class="nav">
+            <ul class="nav__menu">
+                <li class="nav__item"><a href="index.php" class="nav__link">HOME</a></li>
+                <li class="nav__item"><a href="signup.php" class="nav__link">ユーザー登録</a></li>
+                <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
+                <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
+                <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
