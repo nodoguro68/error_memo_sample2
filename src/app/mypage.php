993963938434b9e214fb72f456a50363c7cf5ef9
kawada68 <nodoguro68@gmail.com>
2021-11-04 00:25:28 +0900

    マイページ（仮）作成

---
+++ src/app/mypage.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../utility/utility.php';
+require_once '../function/user.php';
+
+$page_title = 'マイページ';
+require_once '../template/head.php';
+require_once '../template/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+        <!-- メモ件数表示 -->
+        <!-- メモ検索バー -->
+        <!-- 自分のメモリスト -->
+        <!-- いいね蘭のメモリスト -->
+        <!-- サイドバー -->
+        <!-- フォルダ追加フォーム -->
+        
+
+    </div>
+
+</main>
+</body>
+
+</html>
