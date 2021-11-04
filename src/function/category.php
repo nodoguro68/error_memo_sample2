66fc2a0412d26f6c14beb53d8d8f515b3afa92af
kawada68 <nodoguro68@gmail.com>
2021-11-05 01:03:14 +0900

    createCategory関数作成

---
+++ src/function/category.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * カテゴリー追加
+ * @param array $err_msg
+ * @param int $user_id
+ * @param string $category
+ */
+function createCategory(&$err_msg, $user_id, $category) {
+
+    try {
+        $dbh = dbConnect();
+        $sql = 'INSERT INTO categories (user_id, title, created_at) VALUES(:user_id, :title, :created_at)';
+
+        $data = array(
+            ':user_id' => $user_id,
+            ':title' => $category,
+            ':created_at' => date('Y-m-d H:i:s'),
+        );
+
+        if (execute($dbh, $sql, $data)) {
+            
+            return;
+        }
+
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
