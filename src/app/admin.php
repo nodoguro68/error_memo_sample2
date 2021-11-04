ee1f9b559233ebc12b8cdcb3727223f222ade88a
kawada68 <nodoguro68@gmail.com>
2021-11-05 01:00:51 +0900

    カテゴリー追加機能作成

--- src/app/admin.php
+++ src/app/admin.php
@@ -1,7 +1,31 @@
 <?php
 
 require_once '../utility/utility.php';
-require_once '../function/user.php';
+require_once '../function/category.php';
+
+$user_id = $_SESSION['admin_user_id'];
+
+
+if(!empty($_POST)) {
+
+    if(!empty($_POST['create_category'])){
+        
+        $category = trim(filter_input(INPUT_POST, 'create_category'));
+
+        validMaxLen($err_msg, $category, 'common');
+
+        if(empty($err_msg)) {
+
+            createCategory($err_msg, $user_id, $category);
+
+            header('Location: admin.php');
+        }
+    }
+
+
+}
+
+
 
 $page_title = '管理画面';
 require_once '../template/head.php';
@@ -16,6 +40,8 @@ require_once '../template/header.php';
                 <li class="sidebar-list__item"><a href=".php" class="sidebar-list__link">CSV</a></li>
             </ul>
         </aside>
+
+        <?php include_once '../template/err-msg_area.php'; ?>
 
         <form method="post" action="" class="category-form">
             <input type="text" name="create_category" class="form__input" placeholder="カテゴリーを入力">
