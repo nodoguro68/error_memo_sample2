266c8762e98a2191b16f742382a7501807b30bf4
kawada68 <nodoguro68@gmail.com>
2021-11-04 17:31:04 +0900

    管理画面（仮）作成

---
+++ src/app/admin.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../utility/utility.php';
+require_once '../function/user.php';
+
+$page_title = '管理画面';
+require_once '../template/head.php';
+require_once '../template/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+
+
+    </div>
+
+</main>
+</body>
+
+</html>
