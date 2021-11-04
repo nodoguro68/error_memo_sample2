75042a51903ceb7fc02843c7d52904601306b589
kawada68 <nodoguro68@gmail.com>
2021-11-04 17:30:03 +0900

    管理ユーザーか一般ユーザーかによってナビゲーションの項目の表示を変える

--- src/template/header.php
+++ src/template/header.php
@@ -4,11 +4,33 @@
         <nav class="nav">
             <ul class="nav__menu">
                 <li class="nav__item"><a href="index.php" class="nav__link">HOME</a></li>
-                <li class="nav__item"><a href="signup.php" class="nav__link">ユーザー登録</a></li>
-                <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
-                <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
-                <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li>
+                <?php if (!empty($_SESSION['admin_user_id'])) : ?>
+                    <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
+                    <li class="nav__item"><a href="mypage.php" class="nav__link">管理画面</a></li>
+                <?php elseif (!empty($_SESSION['user_id'])) : ?>
+                    <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
+                    <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li>
+                <?php else : ?>
+                    <li class="nav__item"><a href="signup.php" class="nav__link">ユーザー登録</a></li>
+                    <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </div>
 </header>
+
+<!-- 常に表示 -->
+    <!-- HOME -->
+
+<!-- ログインしている場合 -->
+    <!-- ログアウト -->
+
+    <!-- 管理ユーザーの場合 -->
+        <!-- 管理画面 -->
+
+    <!-- 一般ユーザーの場合 -->
+        <!-- マイページ -->
+
+<!-- ログインしていない場合 -->
+    <!-- ユーザー登録 -->
+    <!-- ログイン -->
