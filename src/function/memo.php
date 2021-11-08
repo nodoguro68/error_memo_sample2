85fd962f267edeabefb3849c5075f97d498705dc
kawada68 <nodoguro68@gmail.com>
2021-11-08 18:45:13 +0900

    fetchMemos関数作成

--- src/function/memo.php
+++ src/function/memo.php
@@ -44,3 +44,30 @@ function createMemo(&$err_msg, $user_id, $folder_id, $category_id, $title, $idea
         $err_msg['common'] = ERR_MSG;
     }
 }
+
+
+/**
+ * フォルダ内のメモ全件取得
+ * @param array $err_msg
+ * @param int $user_id
+ * @param int $folder_id
+ * @return mixed
+ */
+function fetchMemos(&$err_msg, $user_id, $folder_id) {
+    try {
+
+        $dbh = dbConnect();
+
+        $sql = 'SELECT memo_id, title, is_published FROM memos WHERE user_id = :user_id AND folder_id = :folder_id AND is_deleted = 0';
+        $data = array(
+            ':user_id' => $user_id,
+            ':folder_id' => $folder_id,
+        );
+
+        $memos = fetchAll($dbh, $sql, $data);
+        return $memos;
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
