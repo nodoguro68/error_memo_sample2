c21d45e7b862cdfb1e3f8d80733d328e6b9424d5
kawada68 <nodoguro68@gmail.com>
2021-11-12 16:05:34 +0900

    いいねボタンを押したときの処理作成

---
+++ src/app/ajax_favorite_memo.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../utility/utility.php';
+require_once '../function/memo.php';
+
+if(isset($_POST['memoId']) && isset($_SESSION['user_id'])) {
+    
+    $memo_id = (int)filter_input(INPUT_POST, 'memoId');
+    $user_id = $_SESSION['user_id'];
+
+    try {
+
+        $resultCount = checkFavoriteMemo($memo_id, $user_id);
+
+        if (!empty($resultCount)) {
+
+            deleteFavoriteMemo($memo_id, $user_id);
+
+        } else {
+
+            createFavoriteMemo($memo_id, $user_id);
+
+        }
+
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
