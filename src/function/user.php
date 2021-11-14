5df16868787b385c294a84266ea86f11bcc460bd
kawada68 <nodoguro68@gmail.com>
2021-11-14 22:34:14 +0900

    createUserの中でadmin_flagによって$dataの中身も分岐させる

--- src/function/user.php
+++ src/function/user.php
@@ -18,21 +18,27 @@ function createUser(&$err_msg, $mail_address, $password, $admin_flag)
             
             $authority = 100;
             $sql = 'INSERT INTO users (authority, mail_address, password, login_time, created_at) VALUES(:authority, :mail_address, :password, :login_time, :created_at)';
+            $data = array(
+                ':authority' => $authority,
+                ':mail_address' => $mail_address,
+                ':password' => password_hash($password, PASSWORD_DEFAULT),
+                ':login_time' => date('Y-m-d H:i:s'),
+                ':created_at' => date('Y-m-d H:i:s'),
+            );
 
 
         } else {
 
             $authority = 1;
             $sql = 'INSERT INTO users (mail_address, password, login_time, created_at) VALUES(:mail_address, :password, :login_time, :created_at)';
+            $data = array(
+                ':mail_address' => $mail_address,
+                ':password' => password_hash($password, PASSWORD_DEFAULT),
+                ':login_time' => date('Y-m-d H:i:s'),
+                ':created_at' => date('Y-m-d H:i:s'),
+            );
         }
         
-        $data = array(
-            ':authority' => $authority,
-            ':mail_address' => $mail_address,
-            ':password' => password_hash($password, PASSWORD_DEFAULT),
-            ':login_time' => date('Y-m-d H:i:s'),
-            ':created_at' => date('Y-m-d H:i:s'),
-        );
 
         if (execute($dbh, $sql, $data)) {
 
@@ -124,5 +130,89 @@ function login(&$err_msg, $mail_address, $password, $pass_save, $admin_flag)
 
     } else {
         $err_msg['common'] = ERR_MSG_LOGIN;
+    }
+}
+
+/**
+ * パスワード取得
+ * 
+ * @param array $err_msg
+ * @param int $user_id
+ * @return 
+ */
+function fetchPass(&$err_msg, $user_id)
+{
+    try {
+
+        $dbh = dbConnect();
+
+        $sql = 'SELECT password FROM users WHERE user_id = :user_id AND is_deleted = 0';
+        $data = array(':user_id' => $user_id);
+
+        $user_data = fetch($dbh, $sql, $data);
+        return $user_data;
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
+/**
+ * 退会
+ * 
+ */
+function signout(&$err_msg, $tables, $user_id, $admin_flag) {
+
+    try {
+    
+        $dbh = dbConnect();
+        $dbh->beginTransaction();
+
+        foreach ($tables as $table) {
+            deleteData($dbh, $table, $user_id, $admin_flag);
+        }
+
+        $_SESSION = array();
+        session_destroy();
+
+        $dbh->commit();
+
+    } catch (Exception $e) {
+
+        $dbh->rollBack();
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
+/**
+ * 論理削除
+ * 
+ * @param object $dbh
+ * @param string $table_name
+ * @param int $user_id
+ * @param bool $admin_flag
+ */
+function deleteData($dbh, $table_name, $user_id, $admin_flag)
+{
+    if($admin_flag) {
+        $sql = 'UPDATE users SET is_deleted = 1 WHERE user_id = :user_id AND authority = 100 AND is_deleted = 0';
+
+    } else {
+
+        if ($table_name === 'favorite_memos') {
+            $sql = 'DELETE FROM ' . $table_name . ' WHERE user_id = :user_id';
+    
+        } else {
+            $sql = 'UPDATE ' . $table_name . ' SET is_deleted = 1 WHERE user_id = :user_id AND is_deleted = 0';
+    
+        }
+    }
+    $data = array(
+        ':user_id' => $user_id,
+    );
+
+    if(execute($dbh, $sql, $data)) {
+        return true;
     }
 }
